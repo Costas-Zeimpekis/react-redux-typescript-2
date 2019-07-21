2b6c52f0e1ce9651d4ad43c7aae1b2a302de75ad
Costas-Zeimpekis <costas@peliba.com>
2019-07-21 12:16:44 +0300

    fix: remove App.css there is no need for it

--- src/container/Posts/Post/Post.tsx
+++ src/container/Posts/Post/Post.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { PostProps } from '../../../MyTypes';
+import { PostProps } from '../../../myTypes';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
